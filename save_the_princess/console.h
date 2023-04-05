602f0f097f7191c6a3075e0e58faaaa59414fc9e
parkhwakyoung <firenoah.m@gmail.com>
2023-04-05 19:14:36 +0900

    총알 player움직임 구현

---
+++ save_the_princess/console.h
@@ -0,0 +1,21 @@
+#include <windows.h>
+
+void SetColor(unsigned char _BgColor, unsigned char _TextColor)
+{
+	if (_BgColor > 15 || _TextColor > 15)
+		return;
+
+	unsigned short ColorNum = (_BgColor << 4) | _TextColor;
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), ColorNum);
+}
+void Clearscreen(void)
+{
+	system("cls");
+}
+
+void gotoxy(int _x, int _y)
+{
+	COORD pos = { _x,_y };
+
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
+}
--- save_the_princess/main.cpp
+++ save_the_princess/main.cpp
@@ -1,5 +1,91 @@
 #include <stdio.h>
+#include <Windows.h>
+#include <stdlib.h>
+#include <time.h>
+#include "console.h"
+
+struct Player {
+    int speed;
+    float x, y;
+    int life;
+};
+
+struct Bullet {
+    int x, y;
+    int is_fired;   //발사 여부
+};
+
+const int BULLET_NUM = 50;
 
 int main() {
 
+
+    struct Player player;
+    player.speed = 10;      //TODO: player_speed해결하기.
+    player.x = 60;
+    player.y = 25;
+    player.life = 3;
+
+    struct Bullet bullet;
+    bullet.is_fired = 0;
+    bullet.x = 0;
+    bullet.y = 0;
+
+    long start_time = clock();   // 게임 시작 시간
+    long spent_time;         // 게임 진행 시간
+    int fired_time = 0;
+
+    srand(time(NULL));
+
+    while (1) {
+
+        Clearscreen();
+        if (GetAsyncKeyState(VK_LEFT)) {
+            player.x--;
+            if (player.x < 0) {
+                player.x = 0;
+            }
+        }
+
+        if (GetAsyncKeyState(VK_RIGHT)) {
+            player.x++;
+            if (player.x > 119) {
+                player.x = 119;
+            }
+        }
+        if (GetAsyncKeyState(VK_UP)) {
+            player.y--;
+            if (player.y < 0) {
+                player.y = 0;
+            }
+        }
+        if (GetAsyncKeyState(VK_DOWN)) {
+            player.y++;
+            if (player.y > 29) {
+                player.y = 29;
+            }
+        }
+        if (GetAsyncKeyState(VK_SPACE)) {
+            bullet.x = player.x;
+            bullet.y = player.y - 1;
+            bullet.is_fired = 1;   //화살이 발사 될때
+
+        }
+        if (bullet.is_fired)   //화살 발사 여부
+        {
+            gotoxy(bullet.x, bullet.y);
+            printf("▲");
+            bullet.y--;
+            if (bullet.y < 0) {
+                bullet.is_fired = 0;
+            }
+        }
+        gotoxy(player.x, player.y);
+        printf("■");
+        Sleep(30);
+
+    }
+
+    system("pause");
+    return 0;
 }
